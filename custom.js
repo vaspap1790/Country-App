1428fe72f8ccfce07fadb4589e42459e884a8b0c
vaspap1790 <vaspap1790@gmail.com>
2019-10-26 21:11:54 +0300

    alpha

--- custom.js
+++ custom.js
@@ -1,6 +1,9 @@
 // Map
-function successMap(position) {
-    console.log(position);
+
+const displayMap = (lon, lat, zoom) => {
+
+    document.querySelector("#map").innerHTML = "";
+
     var map = new ol.Map({
         target: 'map',
         layers: [
@@ -9,42 +12,65 @@ function successMap(position) {
             })
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([position.coords.longitude, position.coords.latitude]),
-            zoom: 5
+            center: ol.proj.fromLonLat([lon, lat]),
+            zoom: zoom
         })
     });
 }
 
-function errorMap(error) {
-    console.log(error);
-}
+const lon = 20;
+const lat = 40;
+const zoom = 6;
 
-if (navigator.geolocation) {
-    console.log("ok");
-    navigator.geolocation.getCurrentPosition(successMap, errorMap);
-} else {
-    console.log("not ok");
-}
+displayMap(lon, lat, zoom);
+
 
 
 // AJAX
 
-const URL = "";
 
 function handleResponse(response) {
     return response.json();
 };
 
-function successAPI(data) {
-    console.log(data);
+const success = (data) => {
+
+    const countrydata = data[0];
+    const name = document.querySelector("#name");
+    const capital = document.querySelector("#capital");
+    const population = document.querySelector("#population");
+    const flag = document.querySelector("#flag");
+    const btn = document.querySelector("#btn");
+
+    name.textContent = "Name: " + countrydata.name;
+    capital.textContent = "Capital: " + countrydata.capital;
+    population.textContent = "Population: " + countrydata.population;
+    flag.src = countrydata.flag;
+    const lat = countrydata.latlng[0];
+    const lon = countrydata.latlng[1];
+    displayMap(lon, lat, 5);
 
 };
 
-function errorAPI(error) {
-    console.log(error);
+const error = (error) => {
+    console.error("Fetch error", error);
 };
 
-fetch(URL)
-    .then(handleResponse)
-    .then(successAPI)
-    .catch(errorAPI);
+
+
+const ajaxCall = () => {
+
+    const country = document.querySelector("#country").value;
+    console.log(country);
+
+    const URL = `https://restcountries.eu/rest/v2/name/${country}`;
+
+    fetch(URL)
+        .then(handleResponse)
+        .then(success)
+        .catch(error);
+}
+
+btn.addEventListener("click", ajaxCall);
+
+
